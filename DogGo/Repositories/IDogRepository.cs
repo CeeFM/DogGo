55e190ff31b31386fb862534f18d3674bd8aab8a
Colin Matteson <seefm@proton.me>
2024-01-12 21:38:05 -0500

    completes first exercise at bottom of chapter 4 of MVC/C# book 2 in newforce backend curriculum

---
+++ DogGo/Repositories/IDogRepository.cs
@@ -0,0 +1,16 @@
+﻿using DogGo.Models;
+
+namespace DogGo.Repositories
+{
+    public interface IDogRepository
+    {
+
+        List<Dog> GetAllDogs();
+        Dog GetDogById(int id);
+        void DeleteDog(int dogId);
+        void UpdateDog(Dog dog);
+        void AddDog(Dog dog);
+        List<Dog> GetDogsByOwnerId(int ownerId);
+
+    }
+}
--- DogGo/Repositories/IOwnerRepository.cs
+++ DogGo/Repositories/IOwnerRepository.cs
@@ -6,5 +6,8 @@ namespace DogGo.Repositories
     {
         List<Owner> GetAllOwners();
         Owner GetOwnerById(int id);
+        void DeleteOwner(int ownerId);
+        void UpdateOwner(Owner owner);
+        void AddOwner(Owner owner);
     }
 }
